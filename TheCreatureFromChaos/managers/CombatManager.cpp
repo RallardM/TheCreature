879fd1bae9fcaf57eadae4afdc7d79e2124cd7d1
Remiam <rallarm@gmail.com>
2023-05-11 06:58:51 -0400

    save last modif

--- TheCreatureFromChaos/managers/CombatManager.cpp
+++ TheCreatureFromChaos/managers/CombatManager.cpp
@@ -201,7 +201,7 @@ void CombatManager::TryToFlee()
 	// generate a random number between 0 and 3 (inclusive)
 	int choice = rand() % 4;
 
-	switch (0) // TODO: put back 'choice' after debug
+	switch (choice) // TODO: put back 'choice' after debug
 	{
 	case 0:
 		// Handle fleeing backwards
